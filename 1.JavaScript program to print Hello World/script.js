e3e6dad72a99174606aef35178b57e5dff19e1d2
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-02-23 13:12:57 +0545

    JavaScript program to sum of two number

--- 1.JavaScript program to print Hello World/script.js
+++ 1.JavaScript program to print Hello World/script.js
@@ -1,1 +1,53 @@
-console.log("Hello World");
+// let js = "amazing";
+// console.log(`JavaScript is fun ${js}`);
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is fun");
+// console.log(10 + 20 - 10);
+// // console.log("JONAS");
+// let FirstName = "Bipin";
+// console.log(FirstName);
+// console.log(FirstName);
+// console.log(FirstName);
+
+// MyFirsrJob = "Programmer";
+// MyCurrentJob = "Teacher";
+// console.log(myFirstjob);
+
+// let JavaScriptisfun = true;
+// console.log(JavaScriptisfun);
+// console.log(typeof true);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1999;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const FirstName = "Bipin";
+const LastName = "Adhikari";
+console.log(FirstName + " " + LastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+let X, Y;
+X = Y = 25 - 10 - 5;
+console.log(X, Y);
