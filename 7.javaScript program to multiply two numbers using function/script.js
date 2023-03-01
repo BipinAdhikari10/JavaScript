dcf9b6b3ff515905a0b81b706613bac9230a489d
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-01 16:47:36 +0545

    javaScript program to subtract two numbers using function

--- 7.javaScript program to multiply two numbers using function/script.js
+++ 7.javaScript program to multiply two numbers using function/script.js
@@ -1,5 +1,20 @@
-function multiplyNumber(Num1, Num2) {
-  return Num1 * Num2;
+// function multiplyNumber(Num1, Num2) {
+//   return Num1 * Num2;
+// }
+// let result = multiplyNumber(5, 2);
+// console.log(result);
+
+//Function declaration//
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
-let result = multiplyNumber(5, 2);
-console.log(result);
+const age1 = calcAge1(1991);
+
+// function expression:
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const a = calcAge2;
+const age3 = a(1990);
+console.log(a, age3);
---
+++ 8.javaScript program to subtract two number using function/script.js
@@ -0,0 +1,7 @@
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+// Example usage:
+const result = subtract(10, 5);
+console.log(result); // Output: 5
