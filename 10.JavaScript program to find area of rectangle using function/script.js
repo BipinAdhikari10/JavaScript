74f64ad8874ee993e1baf45d72c81acfb8d38b3e
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-04 16:15:04 +0545

    JavaScript program to find area of rectangle using for loop

--- 10.JavaScript program to find area of rectangle using function/script.js
+++ 10.JavaScript program to find area of rectangle using function/script.js
@@ -1,5 +1,21 @@
 function areaOfrect(length, width) {
   return length * width;
 }
-const result = areaOfrect(10, 20);
-console.log(result);
+result = areaOfrect(5, 6);
+console.log(`The area of rectangle is ${result}`);
+
+//Expression function
+
+const areaOfrect2 = function (length, width) {
+  return length * width;
+};
+const areaOfrect3 = areaOfrect2;
+const areaOfrect4 = areaOfrect3(40, 10);
+console.log(`The area of rectangle is ${areaOfrect4}`);
+
+const areaOfrect5 = (length, width) => length * width;
+
+const areaOfrect6 = areaOfrect5;
+
+const areaOfrect7 = areaOfrect6(20, 10);
+console.log(`The area of rectangle is ${areaOfrect7}`);
