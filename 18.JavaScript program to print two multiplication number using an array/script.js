a235d78645f39be179aa5f0c8578ed4648863d69
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-11 18:12:00 +0545

    JavaScript program to print two multiplication number using an array

---
+++ 18.JavaScript program to print two multiplication number using an array/script.js
@@ -0,0 +1,10 @@
+// create an array with two numbers
+const numbers = [5, 7];
+
+// multiply the two numbers and store the result in a variable
+const result = numbers[0] * numbers[1];
+
+// print the result to the console
+console.log(
+  `The multiplication of ${numbers[0]} and ${numbers[1]} is ${result}.`
+);
