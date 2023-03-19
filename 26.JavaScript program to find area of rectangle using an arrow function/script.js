965061beeb1bf79494cfd787d6ba2f955b75f7d1
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-19 16:37:17 +0545

    JavaScript program to find area of rectangle using an arrow function

---
+++ 26.JavaScript program to find area of rectangle using an arrow function/script.js
@@ -0,0 +1,10 @@
+const calculateRectangleArea = (length, width) => {
+  return length * width;
+};
+
+// example usage
+const length = 5;
+const width = 3;
+const area = calculateRectangleArea(length, width);
+
+console.log(`The area of the rectangle is ${area} square units.`);
