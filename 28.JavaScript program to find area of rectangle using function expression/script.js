da255f32a37f7b9792ba0acbd8b61071be7bb0e1
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-21 07:09:30 +0545

    JavaScript program to find area of rectangle using function expression

---
+++ 28.JavaScript program to find area of rectangle using function expression/script.js
@@ -0,0 +1,7 @@
+const calculateRectangleArea = function (length, width) {
+  return length * width;
+};
+
+// example usage
+const area = calculateRectangleArea(5, 10);
+console.log("The area of the rectangle is: " + area);
