d583e76b32f127c800865623cca3c3dd43d9ca8f
Bipin Adhikari <adhikaribipin1032@gmail.com>
2023-03-24 16:21:04 +0545

    JavaScript program to find area of triangle

--- 27.JavaScript program to find area of circle uisng an arrow function/script.js
+++ 27.JavaScript program to find area of circle uisng an arrow function/script.js
@@ -1,6 +1,20 @@
-const calculateCircleArea = (radius) => {
-  const area = Math.PI * radius * radius;
-  return area;
-};
+money = 0;
 
-console.log(calculateCircleArea(5)); // Output: 78.53981633974483
+if (money) {
+  console.log("dont spend it all");
+} else {
+  console.log("You need a job");
+}
+
+let height = 10;
+if (height) {
+  console.log("Yah height is defined");
+} else {
+  console.log("Height is undefined");
+}
+
+const age = 23;
+
+age >= 18
+  ? console.log(`You can drink wine`)
+  : console.log(`You can drink water`);
