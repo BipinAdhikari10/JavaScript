4ea0c9a8c8b3ebcf3629d5f4334a5b837bfddf68
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-27 15:34:12 +0545

    DOM practice

--- 36.DOM manupalation/script.js
+++ 36.DOM manupalation/script.js
@@ -1,1 +1,65 @@
-console.log("Hello World");
+// let submitButton1 = document.getElementsByClassName("btn")[0];
+// console.log(submitButton1);
+
+// let submitButton2 = document.getElementsByTagName("button")[0];
+// console.log(submitButton2);
+
+// let paragraph = document.getElementById("text");
+// console.log(paragraph);
+
+// let paragraph1 = document.getElementsByClassName("text-para")[0];
+// console.log(paragraph1);
+
+// let paragaraph2 = document.getElementsByTagName("p")[0];
+// console.log(paragaraph2);
+
+// let submitButton = document.getElementById("submit-btn");
+// submitButton.addEventListener("click", function () {
+//   let inputValue = document.getElementById("address").value;
+
+//   let paragraph = document.getElementById("text");
+//   paragraph.innerText = inputValue;
+// });
+
+// let submitButton = document.getElementById("submit-btn");
+// submitButton.addEventListener("click", function () {
+//   let inputValue = document.getElementById("address").value;
+//   let paragraph = document.getElementById("text");
+//   paragraph.innerText = inputValue;
+// });
+
+// let submitButton = document.getElementById("submit-btn");
+// submitButton.addEventListener("click", function () {
+//   let inputValue = document.getElementById("address").value;
+//   let paragraph = document.getElementById("text");
+//   paragraph.innerText = inputValue;
+// });
+// let submitButton = document.getElementById("submit-btn");
+// // console.log(submitButton
+// submitButton.addEventListener("click", function () {
+//   let inputValue = document.getElementById("address").value;
+//   //   console.log(inputValue);
+//   let paragraph = document.getElementById("text");
+//   paragraph.innerText = inputValue;
+// });
+
+// let submitButton = document.getElementById("submit-btn");
+// submitButton.addEventListener("click", function () {
+//   let inputValue = document.getElementById("address").value;
+//   let paragraph = document.getElementById("text");
+//   paragraph.innerText = inputValue;
+// });
+
+// let submitButton = document.getElementById("submit-btn");
+// submitButton.addEventListener("click", function () {
+//   let inputValue = document.getElementById("address").value;
+//   let paragraph = document.getElementById("text");
+//   paragraph.innerText = inputValue;
+// });
+
+let submitButton = document.getElementById("submit-btn");
+submitButton.addEventListener("click", function () {
+  let inputValue = document.getElementById("address").value;
+  let paragraph = document.getElementById("text");
+  paragraph.innerText = inputValue;
+});
