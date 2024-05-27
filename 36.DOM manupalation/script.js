0bda84872575573baa05fa6a31e399480a33e1f7
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-27 18:54:20 +0545

    Submit form with js practice

--- 36.DOM manupalation/script.js
+++ 36.DOM manupalation/script.js
@@ -63,3 +63,25 @@ submitButton.addEventListener("click", function () {
   let paragraph = document.getElementById("text");
   paragraph.innerText = inputValue;
 });
+
+let submitButton1 = document.getElementById("submit-btn");
+
+submitButton1.addEventListener("click", function () {
+  let input = document.getElementById("name").value;
+  let para = document.getElementById("text-paragraph");
+  para.innerText = input;
+});
+
+let clickButton = document.getElementById("submit-btn");
+clickButton.addEventListener("click", function () {
+  let enterValue = document.getElementById("email").value;
+  let words = document.getElementById("words-text");
+  words.innerText = enterValue;
+});
+
+let clickNumber = document.getElementById("submit-btn");
+clickNumber.addEventListener("click", function () {
+  let enterNumber = document.getElementById("number").value;
+  let number = document.getElementById("mobile-numbers");
+  number.innerText = enterNumber;
+});
