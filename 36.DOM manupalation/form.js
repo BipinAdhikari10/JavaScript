ba60261114b2d4e52268a39c3c4589ff58e5d6f7
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-28 19:15:58 +0545

    append child in js

--- 36.DOM manupalation/form.js
+++ 36.DOM manupalation/form.js
@@ -42,6 +42,7 @@ registrationForm.addEventListener("submit", (event) => {
   document.getElementById("name").value = "";
   document.getElementById("address").value = "";
   document.getElementById("phone").value = "";
+
   document.getElementById("email").value = "bipin@gmail.com";
   document.getElementById("password").value = "";
   document.getElementById("zip").value = "";
@@ -56,3 +57,75 @@ registrationForm.addEventListener("submit", (event) => {
     submittedData[0].password;
   document.getElementById("table-zip").innerText = submittedData[0].zip;
 });
+
+// const child = document.createElement("p");
+
+// const parent = document.getElementById("table-container");
+
+// const text = document.createTextNode("This is paragraph");
+// child.appendChild(text);
+
+// parent.appendChild(child);
+
+// console.log(parent);
+// console.log(child);
+
+{
+  /* <p>This is paragraph</p> */
+}
+
+// let paragraph = document.createElement("p");
+// let text = document.createTextNode("This is me");
+// paragraph.appendChild(text);
+
+// document.getElementById("table-container").appendChild(paragraph);
+// console.log(paragraph);
+
+// let paragraph = document.createElement("p");
+// // console.log(paragraph);
+// let text = document.createTextNode("This is me");
+// // console.log(text);
+// paragraph.appendChild(text);
+// document.getElementById("table-container").appendChild(paragraph);
+// // console.log(paragraph);
+
+// let ul = document.createElement("ul");
+// ul.setAttribute("id", "links");
+// ul.setAttribute("class", "links");
+
+// console.log(ul);
+
+// let li = document.createElement("li");
+// let text = document.createTextNode("Dog");
+// li.appendChild(text);
+
+// let secondLi = document.createElement("li");
+// let secondText = document.createTextNode("Elephant");
+// secondLi.appendChild(secondText);
+
+// secondLi.addEventListener("click", function () {
+//   console.log("Hello");
+//   document.body.style.backgroundColor = "red";
+// });
+
+// li.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "white";
+// });
+// ul.appendChild(li);
+// ul.appendChild(secondLi);
+
+// let tableContainer = document.getElementById("table-container");
+// tableContainer.appendChild(ul);
+
+let ul = document.createElement("ul");
+ul.setAttribute("class", "links");
+ul.setAttribute("id", "link");
+console.log(ul);
+let li = document.createElement("li");
+let text = document.createTextNode("dog");
+li.appendChild(text);
+
+ul.appendChild(li);
+
+let tableContainer = document.getElementById("table-container");
+tableContainer.appendChild(ul);
