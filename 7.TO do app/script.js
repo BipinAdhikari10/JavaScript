507d5a2b9ce2abcba1e830b2a76a83d84da34d46
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-30 16:14:33 +0545

    To do app edit and delete practice

--- 7.TO do app/script.js
+++ 7.TO do app/script.js
@@ -1,34 +1,53 @@
 let inputTagName = document.getElementById("username");
-
-let addButton = document.getElementById("addUser");
+let userButton = document.getElementById("addUser");
 let tableBody = document.getElementById("records");
 
-tableBody = innerHTMl = "";
+tableBody.innerHTML = "";
 
 let submittedData = [];
 
-addButton.addEventListener("click", () => {
-  let EnteredName = inputTagName.value;
+let edit_index = null;
+
+userButton.addEventListener("click", () => {
+  let enteredName = inputTagName.value;
 
   let submitForm = {
-    name: EnteredName,
+    name: enteredName,
   };
-  submittedData.push(submitForm);
-  html = "";
+  if (edit_index !== null) {
+    // Edit case
+    submittedData.splice(edit_index, 1, submitForm);
+    edit_index = null;
+  } else {
+    // Add case
+    submittedData.push(submitForm);
+  }
+  (inputTagName.value = ""), (userButton.innerText = "Add User");
+  displayTable();
 });
+function displayTable() {
+  let html = "";
+  submittedData.forEach(function (user, index) {
+    html += ` <tr>
+    <td>${index + 1}</td>
+    <td>${user.name}</td>
+    <td>
+      <i class="btn text-white fa fa-edit btn-info mx-2" onclick='editInformation(${index})'></i>
+      <i class="btn btn-danger text-white fa fa-trash" onclick="deleteInformation(${index})"></i>
+    </td>
+  </tr>`;
+  });
+  tableBody.innerHTML = html;
+}
+function editInformation(index) {
+  console.log(index);
+  edit_index = index;
+  inputTagName.value = submittedData[index].name;
+  userButton.innerText = "Save edited data";
+}
 
-function displayTableInformation(){
-    submittedData.forEach(user,index){
-html+=`
-<tr>
-                  <td>1</td>
-                  <td>Shiva</td>
-                  <td>
-                    <i class="btn text-white fa fa-edit btn-info mx-2"></i>
-                    <i class="btn btn-danger text-white fa fa-trash"></i>
-                  </td>
-                </tr>
-`
-    }
-    tableBody.innerHTML=html;
+function deleteInformation(index) {
+  submittedData.splice(index, 2);
+
+  displayTable();
 }
