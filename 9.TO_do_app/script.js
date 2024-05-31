9a01477f91b03e52fbdb9c6dc874d97c5f78804a
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-31 10:27:27 +0545

    to do app practice

--- 9.TO_do_app/script.js
+++ 9.TO_do_app/script.js
@@ -1,5 +1,5 @@
 let inputTagName = document.getElementById("username");
-let userBUtton = document.getElementById("addUser");
+let userButton = document.getElementById("addUser");
 let tableBody = document.getElementById("records");
 
 tableBody.innerHTML = "";
@@ -8,7 +8,7 @@ let submittedData = [];
 
 let edit_index = null;
 
-userBUtton.addEventListener("click", () => {
+userButton.addEventListener("click", () => {
   let enteredName = inputTagName.value;
 
   let submitForm = {
@@ -21,32 +21,29 @@ userBUtton.addEventListener("click", () => {
     submittedData.push(submitForm);
   }
   inputTagName.value = "";
-  userBUtton.innerText = "Add User";
-
+  userButton.innerText = "Add User";
   displayTable();
 });
 function displayTable() {
-  let html = "";
+  html = "";
   submittedData.forEach(function (user, index) {
-    html += `<tr>
-        <td>${index + 1}</td>
+    html += ` <tr>
+        <td>${index}</td>
         <td>${user.name}</td>
         <td>
-          <i class="btn text-white fa fa-edit btn-info mx-2" onclick='editInformation(${index})'></i>
-          <i class="btn btn-danger text-white fa fa-trash" onclick='deleteInformation(${index})'></i>
+          <i class="btn text-white fa fa-edit btn-info mx-2" onclick='editInformation (${index})'></i>
+          <i class="btn btn-danger text-white fa fa-trash"onclick= 'deleteInformation (${index})'></i>
         </td>
       </tr>`;
   });
   tableBody.innerHTML = html;
 }
-
 function editInformation(index) {
   edit_index = index;
   inputTagName.value = submittedData[index].name;
-  userBUtton.innerText = "Save edited text";
+  userButton.innerText = "Save edited data";
 }
 function deleteInformation(index) {
   submittedData.splice(index, 1);
-
   displayTable();
 }
