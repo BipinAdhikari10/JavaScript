dd72bfe14af55c1c81e6e6cc7d7caa841bb92481
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-31 09:56:27 +0545

    To do app practice

--- 9.TO_do_app/script.js
+++ 9.TO_do_app/script.js
@@ -1,5 +1,5 @@
 let inputTagName = document.getElementById("username");
-let userButton = document.getElementById("addUser");
+let userBUtton = document.getElementById("addUser");
 let tableBody = document.getElementById("records");
 
 tableBody.innerHTML = "";
@@ -8,46 +8,45 @@ let submittedData = [];
 
 let edit_index = null;
 
-userButton.addEventListener("click", () => {
+userBUtton.addEventListener("click", () => {
   let enteredName = inputTagName.value;
 
   let submitForm = {
     name: enteredName,
   };
-  if (edit_index !== null) {
+  if (edit_index != null) {
     submittedData.splice(edit_index, 1, submitForm);
     edit_index = null;
   } else {
     submittedData.push(submitForm);
   }
   inputTagName.value = "";
-  userButton.innerText = "Add User";
-  console.log(submittedData);
+  userBUtton.innerText = "Add User";
+
   displayTable();
 });
-
 function displayTable() {
   let html = "";
   submittedData.forEach(function (user, index) {
-    html += `  <tr>
-    <td>${index + 1}</td>
-    <td>${user.name}</td>
-    <td>
-      <i class="btn text-white fa fa-edit btn-info mx-2 onclick="editInformation(${index})"></i>
-      <i class="btn btn-danger text-white fa fa-trash" onclick="deleteInformation(${index})"></i>
-    </td>
-  </tr>`;
+    html += `<tr>
+        <td>${index + 1}</td>
+        <td>${user.name}</td>
+        <td>
+          <i class="btn text-white fa fa-edit btn-info mx-2" onclick='editInformation(${index})'></i>
+          <i class="btn btn-danger text-white fa fa-trash" onclick='deleteInformation(${index})'></i>
+        </td>
+      </tr>`;
   });
   tableBody.innerHTML = html;
 }
 
 function editInformation(index) {
   edit_index = index;
-
   inputTagName.value = submittedData[index].name;
-  userButton.innerText = "Save edited data";
+  userBUtton.innerText = "Save edited text";
 }
 function deleteInformation(index) {
   submittedData.splice(index, 1);
+
   displayTable();
 }
