e8ae79ea8bee262aa062b2f25d63461408694f46
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-06-02 20:31:56 +0545

    Array and object advance practice

---
+++ 13.Array and object advance practice/script.js
@@ -0,0 +1,199 @@
+const data = [
+  {
+    country: "Brazil",
+    Religion: [
+      {
+        religion: "Catholic",
+        speak: ["Portuguese", [3, 7], "Carnival"],
+        population: {
+          percentage: "64.6%",
+          major_areas: ["Rio de Janeiro", "Sao Paulo"],
+        },
+      },
+      {
+        religion: "Protestant",
+        speak: ["Portuguese", [2, 5], "Festa Junina"],
+        population: {
+          percentage: "22.2%",
+          major_areas: ["Bahia", "Brasilia"],
+        },
+      },
+      {
+        religion: "Spiritist",
+        speak: [
+          "Portuguese",
+          {
+            count: "2.2%",
+            festival: "Day of the Dead",
+            centers: [
+              {
+                name: "Spiritist Center of Sao Paulo",
+                city: "Sao Paulo",
+                capacity: 300,
+              },
+              {
+                name: "Spiritist Union of Rio",
+                city: "Rio de Janeiro",
+                capacity: 200,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    Education: {
+      primary: "4 years",
+      secondary: "4 years",
+      higher_secondary: "3 years",
+      universities: [
+        {
+          name: "University of Sao Paulo",
+          founded: 1934,
+          students: 90000,
+        },
+        {
+          name: "Federal University of Rio de Janeiro",
+          founded: 1920,
+          students: 65000,
+        },
+      ],
+    },
+  },
+  "Good luck",
+  "Artificial Intelligence",
+  {
+    country: "Japan",
+    Religion: [
+      {
+        religion: "Shinto",
+        population: {
+          percentage: "48%",
+          major_areas: ["Tokyo", "Kyoto"],
+        },
+      },
+      {
+        religion: "Buddhism",
+        population: {
+          percentage: "46%",
+          major_areas: ["Osaka", "Nara"],
+        },
+      },
+    ],
+    Athletes: [
+      {
+        name: "Shohei Ohtani",
+        Sport: ["Baseball", "Pitcher"],
+        Awards: [
+          {
+            MLB: 1,
+            NPB: 1,
+            WBC: 1,
+          },
+        ],
+        statistics: {
+          games: 150,
+          home_runs: 45,
+          ERA: 2.52,
+        },
+      },
+      {
+        name: "Naomi Osaka",
+        Sport: ["Tennis", "Singles"],
+        Awards: [
+          {
+            GrandSlams: 4,
+            WTA: 7,
+            Olympics: 1,
+          },
+        ],
+        statistics: {
+          matches: 100,
+          wins: 85,
+          losses: 15,
+        },
+      },
+    ],
+    Education: {
+      primary: "6 years",
+      secondary: "3 years",
+      higher_secondary: "3 years",
+      universities: [
+        {
+          name: "University of Tokyo",
+          founded: 1877,
+          students: 30000,
+        },
+        {
+          name: "Kyoto University",
+          founded: 1897,
+          students: 22000,
+        },
+      ],
+    },
+  },
+  {
+    country: "Australia",
+    Religion: [
+      {
+        religion: "Christianity",
+        population: {
+          percentage: "52%",
+          major_areas: ["Sydney", "Melbourne"],
+        },
+      },
+      {
+        religion: "No Religion",
+        population: {
+          percentage: "30%",
+          major_areas: ["Brisbane", "Perth"],
+        },
+      },
+    ],
+    Leaders: [
+      {
+        name: "Scott Morrison",
+        term: "2018-2022",
+        party: "Liberal",
+      },
+      {
+        name: "Julia Gillard",
+        term: "2010-2013",
+        party: "Labor",
+        achievements: [
+          "National Broadband Network",
+          "DisabilityCare Australia",
+        ],
+      },
+    ],
+    Sports: {
+      popular_sports: ["Cricket", "Rugby", "Soccer"],
+      major_events: [
+        {
+          name: "Melbourne Cup",
+          sport: "Horse Racing",
+          audience: "10 million",
+        },
+        {
+          name: "Australian Open",
+          sport: "Tennis",
+          audience: "30 million",
+        },
+      ],
+    },
+  },
+];
+
+console.log(data[0].Religion[2].speak[1].centers[1].capacity);
+
+console.log(`I have the data of ${data[0].Religion[0].speak[1][0]} countries
+ ${data[0].country}:
+I will talk about ${data[0].Religion[0].speak[1][0]} religions. They are ${data[0].Religion[0].religion},${data[0].Religion[1].religion},${data[0].Religion[2].religion}.
+The percantage of people who speak ${data[0].Religion[0].speak[0]} is ${data[0].Religion[0].population.percentage}
+The carnival is celeberated by ${data[0].Religion[0].speak[1][0]} to ${data[0].Religion[0].speak[1][1]} people.
+The major area where ${data[0].Religion[0].religion} is done is ${data[0].Religion[0].population.major_areas[0]} and ${data[0].Religion[0].population.major_areas[1]}
+another language is ${data[0].Religion[1].speak[2]}, major cities are ${data[0].Religion[1].population.major_areas[0]} and ${data[0].Religion[1].population.major_areas[1]},
+percentage covered is ${data[0].Religion[1].population.percentage}, another religion is ${data[0].Religion[2].religion} and major centeres are ${data[0].Religion[1].speak[1][0]} and capacity 
+for ${data[0].Religion[2].speak[1].centers[0].name} is ${data[0].Religion[2].speak[1].centers[0].capacity} and ${data[0].Religion[2].speak[1].centers[1].name} is 
+${data[0].Religion[2].speak[1].centers[1].capacity}
+
+`);
--- 9.TO_do_app/script.js
+++ 9.TO_do_app/script.js
@@ -136,76 +136,265 @@ console.log(bipinInfo[0].friends.length);
 //   and their name are ${bipinInfo[0].friends[0].name} and ${bipinInfo[0].friends[1].name}.  lives in ${bipinInfo[0].friends[1].country}`
 // );
 
-const Info = [
+// const Info = [
+//   {
+//     country: "Nepal",
+//     Religion: [
+//       { religion: "Hindu", speak: ["Nepali", [5, 6], "Dashain"] },
+//       { religion: "Buddhist", speak: ["Newari", [10, 15], "Buddha Jayanti"] },
+//       {
+//         religion: "Christian",
+//         speak: [
+//           "English",
+//           {
+//             count: "2%",
+//             festival: "Christmas",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   "All the best",
+//   "Programming",
+//   {
+//     country: "India",
+//     Religion: ["Sikh", "Ishai"],
+//     crickters: [
+//       {
+//         name: "Kholi",
+//         Team: ["India", "RCB"],
+//         Trophy: [
+//           {
+//             country: 5,
+//             RCB: 0,
+//             MI: 0,
+//           },
+//         ],
+//       },
+//       {
+//         name: "Rohit",
+//         Team: ["India", "MI"],
+//         Trophy: [
+//           {
+//             country: 5,
+//             RCB: 0,
+//             MI: 5,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+// console.log(Info[2]);
+
+// console.log(
+//   `I live in ${Info[0].country}.Here, People follow ${Info[0].Religion[0].religion.length} big religion ${Info[0].Religion[0].religion}
+//   ,${Info[0].Religion[1].religion} and ${Info[0].Religion[2].religion} are the big religion,${Info[0].Religion[0].religion}'s speak
+//   ${Info[0].Religion[0].speak[0]} and celebrate ${Info[0].Religion[0].speak[1][0]} to ${Info[0].Religion[0].speak[1][1]} festivals.
+//   ${Info[0].Religion[0].speak[2]} is the main festival.${Info[0].Religion[1].speak[0]} speaked by ${Info[0].Religion[1].religion} Being
+//   ${Info[0].Religion[1].speak[2]}, the biggest festival, they celebrate ${Info[0].Religion[1].speak[1][0]} -${Info[0].Religion[1].speak[1][1]}
+//   festivals.${Info[0].Religion[2].religion} is followed by ${Info[0].Religion[2].speak[1].count} of the people and celebrate ${Info[0].Religion[2].speak[1].festival}
+
+//   ${Info[3].crickters[0].name} and  ${Info[3].crickters[1].name} are the biggest player of ${Info[3].country}.
+//   Both of them are ${Info[0].Religion[0].religion},They play for ${Info[3].crickters[0].Team[1]},${Info[3].crickters[1].Team[1]}
+// and ${Info[3].country}, with ${Info[3].crickters[0].Trophy[0].country} Trophy for India. ${Info[3].crickters[1].name} has
+//  ${Info[3].crickters[1].Trophy[0].MI} IPL trophies whereas ${Info[3].crickters[0].name} has ${Info[3].crickters[1].Trophy[0].RCB}
+//  because he plays for ${Info[3].crickters[0].Team[1]}
+
+//  Wish me ${Info[1]} for  ${Info[2]}
+//  `
+// );
+
+// console.log(Info);
+// console.log(Info[0]);
+// console.log(Info[0].country);
+// console.log(Info[0].Religion.length);
+
+// console.log(Info);
+
+const data = [
   {
-    country: "Nepal",
+    country: "Brazil",
     Religion: [
-      { religion: "Hindu", speak: ["Nepali", [5, 6], "Dashain"] },
-      { religion: "Buddhist", speak: ["Newari", [10, 15], "Buddha Jayanti"] },
       {
-        religion: "Christian",
+        religion: "Catholic",
+        speak: ["Portuguese", [3, 7], "Carnival"],
+        population: {
+          percentage: "64.6%",
+          major_areas: ["Rio de Janeiro", "Sao Paulo"],
+        },
+      },
+      {
+        religion: "Protestant",
+        speak: ["Portuguese", [2, 5], "Festa Junina"],
+        population: {
+          percentage: "22.2%",
+          major_areas: ["Bahia", "Brasilia"],
+        },
+      },
+      {
+        religion: "Spiritist",
         speak: [
-          "English",
+          "Portuguese",
           {
-            count: "2%",
-            festival: "Christmas",
+            count: "2.2%",
+            festival: "Day of the Dead",
+            centers: [
+              {
+                name: "Spiritist Center of Sao Paulo",
+                city: "Sao Paulo",
+                capacity: 300,
+              },
+              {
+                name: "Spiritist Union of Rio",
+                city: "Rio de Janeiro",
+                capacity: 200,
+              },
+            ],
           },
         ],
       },
     ],
+    Education: {
+      primary: "4 years",
+      secondary: "4 years",
+      higher_secondary: "3 years",
+      universities: [
+        {
+          name: "University of Sao Paulo",
+          founded: 1934,
+          students: 90000,
+        },
+        {
+          name: "Federal University of Rio de Janeiro",
+          founded: 1920,
+          students: 65000,
+        },
+      ],
+    },
   },
-  "All the best",
-  "Programming",
+  "Good luck",
+  "Artificial Intelligence",
   {
-    country: "India",
-    Religion: ["Sikh", "Ishai"],
-    crickters: [
+    country: "Japan",
+    Religion: [
       {
-        name: "Kholi",
-        Team: ["India", "RCB"],
-        Trophy: [
-          {
-            country: 5,
-            RCB: 0,
-            MI: 0,
-          },
-        ],
+        religion: "Shinto",
+        population: {
+          percentage: "48%",
+          major_areas: ["Tokyo", "Kyoto"],
+        },
       },
       {
-        name: "Rohit",
-        Team: ["India", "MI"],
-        Trophy: [
+        religion: "Buddhism",
+        population: {
+          percentage: "46%",
+          major_areas: ["Osaka", "Nara"],
+        },
+      },
+    ],
+    Athletes: [
+      {
+        name: "Shohei Ohtani",
+        Sport: ["Baseball", "Pitcher"],
+        Awards: [
           {
-            country: 5,
-            RCB: 0,
-            MI: 5,
+            MLB: 1,
+            NPB: 1,
+            WBC: 1,
           },
+        ],
+        statistics: {
+          games: 150,
+          home_runs: 45,
+          ERA: 2.52,
+        },
+      },
+      {
+        name: "Naomi Osaka",
+        Sport: ["Tennis", "Singles"],
+        Awards: [
+          {
+            GrandSlams: 4,
+            WTA: 7,
+            Olympics: 1,
+          },
+        ],
+        statistics: {
+          matches: 100,
+          wins: 85,
+          losses: 15,
+        },
+      },
+    ],
+    Education: {
+      primary: "6 years",
+      secondary: "3 years",
+      higher_secondary: "3 years",
+      universities: [
+        {
+          name: "University of Tokyo",
+          founded: 1877,
+          students: 30000,
+        },
+        {
+          name: "Kyoto University",
+          founded: 1897,
+          students: 22000,
+        },
+      ],
+    },
+  },
+  {
+    country: "Australia",
+    Religion: [
+      {
+        religion: "Christianity",
+        population: {
+          percentage: "52%",
+          major_areas: ["Sydney", "Melbourne"],
+        },
+      },
+      {
+        religion: "No Religion",
+        population: {
+          percentage: "30%",
+          major_areas: ["Brisbane", "Perth"],
+        },
+      },
+    ],
+    Leaders: [
+      {
+        name: "Scott Morrison",
+        term: "2018-2022",
+        party: "Liberal",
+      },
+      {
+        name: "Julia Gillard",
+        term: "2010-2013",
+        party: "Labor",
+        achievements: [
+          "National Broadband Network",
+          "DisabilityCare Australia",
         ],
       },
     ],
+    Sports: {
+      popular_sports: ["Cricket", "Rugby", "Soccer"],
+      major_events: [
+        {
+          name: "Melbourne Cup",
+          sport: "Horse Racing",
+          audience: "10 million",
+        },
+        {
+          name: "Australian Open",
+          sport: "Tennis",
+          audience: "30 million",
+        },
+      ],
+    },
   },
 ];
-console.log(Info[2]);
 
-console.log(
-  `I live in ${Info[0].country}.Here, People follow ${Info[0].Religion[0].religion.length} big religion ${Info[0].Religion[0].religion}
-  ,${Info[0].Religion[1].religion} and ${Info[0].Religion[2].religion} are the big religion,${Info[0].Religion[0].religion}'s speak 
-  ${Info[0].Religion[0].speak[0]} and celebrate ${Info[0].Religion[0].speak[1][0]} to ${Info[0].Religion[0].speak[1][1]} festivals.
-  ${Info[0].Religion[0].speak[2]} is the main festival.${Info[0].Religion[1].speak[0]} speaked by ${Info[0].Religion[1].religion} Being
-  ${Info[0].Religion[1].speak[2]}, the biggest festival, they celebrate ${Info[0].Religion[1].speak[1][0]} -${Info[0].Religion[1].speak[1][1]}
-  festivals.${Info[0].Religion[2].religion} is followed by ${Info[0].Religion[2].speak[1].count} of the people and celebrate ${Info[0].Religion[2].speak[1].festival}
-  
-
-
-  ${Info[3].crickters[0].name} and  ${Info[3].crickters[1].name} are the biggest player of ${Info[3].country}.
-  Both of them are ${Info[0].Religion[0].religion},They play for ${Info[3].crickters[0].Team[1]},${Info[3].crickters[1].Team[1]}
-and ${Info[3].country}, with ${Info[3].crickters[0].Trophy[0].country} Trophy for India. ${Info[3].crickters[1].name} has
- ${Info[3].crickters[1].Trophy[0].MI} IPL trophies whereas ${Info[3].crickters[0].name} has ${Info[3].crickters[1].Trophy[0].RCB}
- because he plays for ${Info[3].crickters[0].Team[1]}
-
- Wish me ${Info[1]} for  ${Info[2]}
- `
-);
-console.log(Info);
-console.log(Info[0].country);
-console.log(Info[0].Religion);
+console.log(data);
