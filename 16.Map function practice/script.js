bb8b62675f332df5ad616073c817d16cc362f714
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-06-20 17:00:26 +0545

    array map practice

--- 16.Map function practice/script.js
+++ 16.Map function practice/script.js
@@ -1,20 +1,28 @@
-const numbers = [4, 5, 6];
-const squareNumbers = numbers.map((number) => number * number);
-console.log(squareNumbers);
+// const numbers = [4, 5, 6];
+// const squareNumbers = numbers.map((number) => number * number);
+// console.log(squareNumbers);
 
-const array = [1, 2, 3, 4];
-const square = array.map((num) => num * num);
-console.log(square);
+// const array = [1, 2, 3, 4];
+// const square = array.map((num) => num * num);
+// console.log(square);
 
-const arr = [5, 10, 15, 20];
-let a = arr.map((a) => a * a);
-console.log(a);
+// const arr = [5, 10, 15, 20];
+// let a = arr.map((a) => a * a);
+// console.log(a);
 
-const number = [4, 9, 16, 25];
-const newArr = number.map(Math.sqrt);
-console.log(newArr);
+// const number = [4, 9, 16, 25];
+// const newArr = number.map(Math.sqrt);
+// console.log(newArr);
 
-let array1 = [4, 5, 6, 8];
+// let array1 = [4, 5, 6, 8];
 
-let newArray = array1.filter((ele) => ele % 2 === 0);
-console.log(newArray);
+// let newArray = array1.filter((ele) => ele % 2 === 0);
+// console.log(newArray);
+
+// const fruits = ["apple"];
+// const fruit = fruits.map((fruit) => fruit.toUpperCase());
+// console.log(fruit);
+
+let array2 = [2, 4, 6, 8];
+let newArray1 = array2.map((value) => value * 5);
+console.log(newArray1);
