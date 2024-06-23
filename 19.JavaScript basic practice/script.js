0f35a4f54449c037f24a4d21a76bce86e0df1a02
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-06-23 12:13:03 +0545

    JavaScript basic practice

--- 19.JavaScript basic practice/script.js
+++ 19.JavaScript basic practice/script.js
@@ -58,3 +58,58 @@ if (stringNumber !== 5) {
 } else {
   console.log("It's today");
 }
+
+const arr = [1, 2, 3];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+const user = {
+  name: "Bipin",
+  age: 20,
+};
+for (key in user) console.log(key + ":" + user[key]);
+
+const carModels = {
+  model1: "Tesla",
+  model2: "BMW",
+};
+for (key in carModels) {
+  console.log(key + ":" + carModels[key]);
+}
+
+const array = ["apple", "banana", "cherry"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(array[i]);
+}
+
+const students = ["Bipin", "Naran", "Hari", "Shyam"];
+for (element of students) {
+  console.log(element);
+}
+
+let strings1 = "hellobipin";
+for (bipin of strings1) {
+  console.log(bipin);
+}
+
+let set = [1, 2, 10, 3, 4, 5, 6];
+for (ele of set) {
+  console.log(ele);
+}
+
+let studentsName = {
+  name: "Bipin",
+  age: 22,
+};
+for (key in studentsName) {
+  console.log(key + ":" + studentsName[key]);
+}
+
+const salaries = {
+  Bipin: 3000000,
+  Ram: 1000,
+};
+for (key in salaries) {
+  console.log(key + ":" + salaries[key]);
+}
