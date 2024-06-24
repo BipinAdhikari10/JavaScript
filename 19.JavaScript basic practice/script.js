3cb4ef42ae3b5b86f59a37149f5b2f73c3549f85
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-06-24 08:08:33 +0545

    JavaScript for loop

--- 19.JavaScript basic practice/script.js
+++ 19.JavaScript basic practice/script.js
@@ -1,115 +1,146 @@
-// check string or number
+// // check string or number
 
-let quantity = "12";
-console.log(typeof quantity);
+// let quantity = "12";
+// console.log(typeof quantity);
 
-// Change string to number
+// // Change string to number
 
-let string = "122";
-console.log(+string);
+// let string = "122";
+// console.log(string);
 
-let name = "Bipin";
-console.log(+name);
+// let name = "Bipin";
+// console.log(+name);
 
-let strings = "10";
-console.log(strings * 2);
+// let strings = "10";
+// console.log(strings * 2);
 
-// number to string
+// // number to string
 
-let num = 20;
-let num1 = num.toString(8);
-console.log(num1);
+// let num = 20;
+// let num1 = num.toString(8);
+// console.log(num1);
 
-// Push method it is used to add new element at the end of the array
+// // Push method it is used to add new element at the end of the array
 
-// let fruits = ["apple"];
-// fruits.push("banana");
-// console.log(fruits);
+// // let fruits = ["apple"];
+// // fruits.push("banana");
+// // console.log(fruits);
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-let size = fruits.length;
-console.log(size);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let size = fruits.length;
+// console.log(size);
 
-// let age = prompt("Enter your age");
-// if (age > 20) {
-//   console.log("You are eligible to driving");
+// // let age = prompt("Enter your age");
+// // if (age > 20) {
+// //   console.log("You are eligible to driving");
+// // } else {
+// //   console.log("You are not eligible to driving");
+// // }
+
+// let hour = 14;
+
+// if (hour > 10) {
+//   console.log("Good morning");
 // } else {
-//   console.log("You are not eligible to driving");
+//   console.log("Good afternoon");
 // }
 
-let hour = 14;
+// let number = 5;
 
-if (hour > 10) {
-  console.log("Good morning");
-} else {
-  console.log("Good afternoon");
+// let stringNumber = number.toString();
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// if (stringNumber !== 5) {
+//   console.log("Hello");
+// } else if (stringNumber === "5") {
+//   console.log("Hello World");
+// } else {
+//   console.log("It's today");
+// }
+
+// const arr = [1, 2, 3];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// const user = {
+//   name: "Bipin",
+//   age: 20,
+// };
+// for (key in user) console.log(key + ":" + user[key]);
+
+// const carModels = {
+//   model1: "Tesla",
+//   model2: "BMW",
+// };
+// for (key in carModels) {
+//   console.log(key + ":" + carModels[key]);
+// }
+
+// const array = ["apple", "banana", "cherry"];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(array[i]);
+// }
+
+// const students = ["Bipin", "Naran", "Hari", "Shyam"];
+// for (element of students) {
+//   console.log(element);
+// }
+
+// let strings1 = "hellobipin";
+// for (bipin of strings1) {
+//   console.log(bipin);
+// }
+
+// let set = [1, 2, 10, 3, 4, 5, 6];
+// for (ele of set) {
+//   console.log(ele);
+// }
+
+// let studentsName = {
+//   name: "Bipin",
+//   age: 22,
+// };
+// for (key in studentsName) {
+//   console.log(key + ":" + studentsName[key]);
+// }
+
+// const salaries = {
+//   Bipin: 3000000,
+//   Ram: 1000,
+// };
+// for (key in salaries) {
+//   console.log(key + ":" + salaries[key]);
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// // numbers.forEach(function (index) {
+// //   console.log(index);
+// // });
+// numbers.forEach((index) => {
+//   console.log(index);
+// });
+
+// const sum = () => {
+//   console.log(2 + 3);
+// };
+
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+
+// sum();
+
+// for (let i = 1; i <= 7; i++) {
+//   sum();
+// }
+
+function productNum(a, b, c) {
+  let product = a * b * c;
+  console.log(product);
 }
-
-let number = 5;
-
-let stringNumber = number.toString();
-console.log(stringNumber);
-console.log(typeof stringNumber);
-
-if (stringNumber !== 5) {
-  console.log("Hello");
-} else if (stringNumber === "5") {
-  console.log("Hello World");
-} else {
-  console.log("It's today");
-}
-
-const arr = [1, 2, 3];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
-
-const user = {
-  name: "Bipin",
-  age: 20,
-};
-for (key in user) console.log(key + ":" + user[key]);
-
-const carModels = {
-  model1: "Tesla",
-  model2: "BMW",
-};
-for (key in carModels) {
-  console.log(key + ":" + carModels[key]);
-}
-
-const array = ["apple", "banana", "cherry"];
-for (let i = 0; i < arr.length; i++) {
-  console.log(array[i]);
-}
-
-const students = ["Bipin", "Naran", "Hari", "Shyam"];
-for (element of students) {
-  console.log(element);
-}
-
-let strings1 = "hellobipin";
-for (bipin of strings1) {
-  console.log(bipin);
-}
-
-let set = [1, 2, 10, 3, 4, 5, 6];
-for (ele of set) {
-  console.log(ele);
-}
-
-let studentsName = {
-  name: "Bipin",
-  age: 22,
-};
-for (key in studentsName) {
-  console.log(key + ":" + studentsName[key]);
-}
-
-const salaries = {
-  Bipin: 3000000,
-  Ram: 1000,
-};
-for (key in salaries) {
-  console.log(key + ":" + salaries[key]);
-}
+productNum(4, 5, 6);
---
+++ 20.JavaScript for loop/script.js
@@ -0,0 +1,6 @@
+const sum = () => {
+  console.log(5 + 6);
+};
+for (let i = 0; i < 10; i++) {
+  sum();
+}
