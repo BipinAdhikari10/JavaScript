add113076a06ffc8a40c9e6b0b28ff171a46f6c5
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-06-24 08:28:07 +0545

    for loop with function

--- 20.JavaScript for loop/script.js
+++ 20.JavaScript for loop/script.js
@@ -1,6 +1,13 @@
-const sum = () => {
-  console.log(5 + 6);
+// const sum = () => {
+//   console.log(5 + 3);
+// };
+// for (let i = 0; i < 4; i++) {
+//   sum();
+// }
+
+const sum = (num1, num2, num3) => {
+  console.log(num1 + num2 + num3);
 };
-for (let i = 0; i < 10; i++) {
-  sum();
+for (let i = 1; i < 4; i++) {
+  sum(i, i + 1, i + 2);
 }
