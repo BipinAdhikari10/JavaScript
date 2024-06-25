9efed1091b28870e326eadf87f426d9f2be8efc5
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-06-25 12:34:22 +0545

    Prime number using function another way

--- 21.Prime number using function and for loop/script.js
+++ 21.Prime number using function and for loop/script.js
@@ -1,17 +1,53 @@
-const primeNumber = (num) => {
+// // const primeNumber = (num) => {
+//   if (num <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i < Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// let isPrime = primeNumber(23);
+// if (isPrime) {
+//   console.log("Prime number");
+// } else {
+//   console.log("Not prime number");
+// }
+
+// const primeNumber = (num) => {
+//   if (num >= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// isPrime = primeNumber(25);
+// if (isPrime) {
+//   console.log("This is prime number");
+// } else {
+//   console.log("This is not a prime number");
+// }
+
+function primeNumber(num) {
   if (num <= 1) {
     return false;
   }
-  for (let i = 2; i < Math.sqrt(num); i++) {
+  for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       return false;
     }
   }
   return true;
-};
-let isPrime = primeNumber(23);
+}
+const isPrime = primeNumber(29);
 if (isPrime) {
-  console.log("Prime number");
+  console.log("This is prime number");
 } else {
-  console.log("Not prime number");
+  console.log("This is not a prime number");
 }
