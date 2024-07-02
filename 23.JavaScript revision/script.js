3becb9dc6fbfb94ffd52786c2b25e9df8163d6ee
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-02 13:02:26 +0545

    convert string to number

--- 23.JavaScript revision/script.js
+++ 23.JavaScript revision/script.js
@@ -1,8 +1,44 @@
-const a = 15;
-const b = "volvo";
-console.log(a + b);
-console.log(typeof (a + b));
+// const a = "15";
+// const b = "volvo";
+// console.log(a + b);
+// console.log(typeof (a + b));
 
-const x = 20;
-const y = 2;
-console.log(typeof (x + y));
+// const x = 20;
+// const y = 2;
+// console.log(typeof (x + y));
+
+// let c = "tesla";
+// let d = 17;
+// console.log(c + d);
+// console.log(typeof (c + d));
+
+// let a = 15;
+// let b = 20;
+// let c = "tesla";
+// console.log(a + b + c);
+// console.log(typeof (a + b + c));
+
+// let a = "Tesla";
+// let b = 2;
+// let c = 20;
+// console.log(a + b + c);
+// console.log(typeof (a + b + c));
+
+function addSum() {
+  let a = 5;
+  let b = 6;
+  return a + b;
+}
+let sum = addSum();
+console.log(sum);
+
+const quantity = "12";
+console.log(typeof quantity);
+console.log(typeof +quantity);
+console.log(+quantity);
+
+const string = "awosome";
+console.log(+string);
+
+const stringNumber = 20;
+console.log(stringNumber * 2);
