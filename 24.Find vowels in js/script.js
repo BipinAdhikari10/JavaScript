de3655f07df5a219cbb182aaf672652fc4965e00
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-03 11:31:45 +0545

    Vowels count

--- 24.Find vowels in js/script.js
+++ 24.Find vowels in js/script.js
@@ -5,3 +5,21 @@ console.log(fruit);
 let language = ["C", "JavaScript"];
 let languages = language.includes("python");
 console.log(languages);
+
+let vehicles = ["Car", "Van", "Bus"];
+let vehicle = vehicles.includes("Car");
+console.log(vehicle);
+
+const findVowels = (inputString) => {
+  const vowels = "aeiouAEIOU";
+  let vowel = 0;
+  for (char of inputString) {
+    if (vowels.includes(char)) {
+      vowel++;
+    }
+  }
+  return vowel;
+};
+const string = "Helloaeiou";
+const result = findVowels(string);
+console.log(result);
