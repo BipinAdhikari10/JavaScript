a214dda40b2430d19a9e2162ffa4d62796d64e54
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-08 07:49:16 +0545

    Making push function

--- 26.Array function push pop shift unshift splice/script.js
+++ 26.Array function push pop shift unshift splice/script.js
@@ -12,3 +12,10 @@ const findEven = (arr) => {
 const array = [1, 2, 3, 45, 6, 7, 8, 9, 10, 11, 12, 13, 15, 14, 16, 17];
 let evenNumbers = findEven(array);
 console.log(evenNumbers);
+
+const pushArray = (cars, value) => {
+  cars[cars.length] = value;
+};
+cars = ["Tesla", "BMW"];
+pushArray(cars, "Ferrari");
+console.log(cars);
