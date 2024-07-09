d4bbbe2644510469a8fe67b2dd726802b238ef90
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-09 10:53:59 +0545

    array in array practice

--- 27.double loop on array/script.js
+++ 27.double loop on array/script.js
@@ -1,11 +1,24 @@
-let arr = [1, 2, 3, 4, 5, 6];
-let targetSum = 7;
-let pairs = [];
+// let arr = [1, 2, 3, 4, 5, 6];
+// let targetSum = 7;
+// let pairs = [];
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[i] + arr[j] === targetSum) {
+//       pairs.push([arr[i], arr[j]]);
+//     }
+//   }
+// }
+// console.log(pairs);
+
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+let sum = 0;
 for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] + arr[j] === targetSum) {
-      pairs.push([arr[i], arr[j]]);
-    }
+  for (let j = 0; j < arr[i].length; j++) {
+    sum += arr[i][j];
   }
 }
-console.log(pairs);
+console.log(sum);
