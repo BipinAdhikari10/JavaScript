1a5fbdbebb11c6c824a7c53e6e3328233e298fcc
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-10 12:23:34 +0545

    Even number using filter function

--- 28.Reverse and other practice questions using for loop and array/script.js
+++ 28.Reverse and other practice questions using for loop and array/script.js
@@ -20,3 +20,7 @@ const originalString = "Bipin";
 let newString = reversedString(originalString);
 console.log(newString);
 console.log(typeof newString);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 22, 31, 52];
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
+console.log(evenNumbers);
