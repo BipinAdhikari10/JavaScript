ee56631bb696c6a2e523976b448353ac8f0d86aa
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-16 17:53:10 +0545

    multiply number

--- 31.Reverse the given array string using split and join/script.js
+++ 31.Reverse the given array string using split and join/script.js
@@ -38,10 +38,47 @@
 // };
 // printSum(7);
 
-const printMultiply = (num) => {
-  for (let i = 1; i <= 10; i++) {
-    console.log(`The multiplication  of ${num} *  ${i} is ${num * i}`);
-  }
-};
+// const printMultiply = (num) => {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(`The multiplication  of ${num} *  ${i} is ${num * i}`);
+//   }
+// };
 
-printMultiply(5);
+// printMultiply(5);
+
+// const printMultiply = (num) => {
+//   for (let i = 1; i <= num; i++) {
+//     console.log(`The multiplaction table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//       console.log(`${i}*${j}=${i * j}`);
+//     }
+//   }
+// };
+// printMultiply(5);
+
+// const printMultiply = (num) => {
+//   let multiply = [];
+//   for (let i = 1; i <= 10; i++) {
+//     multiply.push(num * i);
+//   }
+//   return multiply;
+// };
+// let number = printMultiply(10);
+// console.log(number);
+
+const printMultipleTable = (num) => {
+  let greaterNum = 0;
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= 10; j++) {
+      let product = i * j;
+      console.log(product);
+      if (product > greaterNum) {
+        greaterNum = product;
+      }
+    }
+  }
+  return greaterNum;
+};
+let number = 3;
+let result = printMultipleTable(number);
+console.log(result);
