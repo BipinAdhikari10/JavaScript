1dfe757632011f33f69da69a942d585a527fa748
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-18 11:24:37 +0545

    slice practice

--- 33.Paalindrome in js/script.js
+++ 33.Paalindrome in js/script.js
@@ -6,7 +6,7 @@ const isPalindrome = (str) => {
 
   return false;
 };
-let strings = "bipin";
+let strings = "mom";
 let value = isPalindrome(strings);
 if (value) {
   console.log("The string is palindrome");
@@ -19,3 +19,14 @@ let string = arr.join("");
 let joinedStrings = string.charAt(0).toUpperCase() + string.slice(1);
 
 console.log(joinedStrings);
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 4);
+console.log(citrus);
+
+const cities = ["Tokyo", "Cairo", "Los Angeles", "Paris", "Seattle"];
+
+const newCityArr = cities.slice(2, 4);
+
+console.log("Original: ", cities);
+console.log("New: ", newCityArr);
