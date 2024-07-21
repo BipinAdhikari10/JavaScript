aa472e04b2a0ecee5299ab6a5ab535b023062dc4
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-21 12:10:09 +0545

    filter method practice

--- 37.for of loop and map filter practice/script.js
+++ 37.for of loop and map filter practice/script.js
@@ -1,11 +1,18 @@
 // Using for of loop
-const findNewArr = (arr) => {
-  let newArr = [];
-  for (let ele of arr) {
-    if (ele > 2 && ele < 10 && ele % 2 === 0) {
-      newArr.push(ele);
-    }
-  }
-  console.log(newArr);
-};
-findNewArr([1, 3, 6, 8, 7, 10]);
+// const findNewArr = (arr) => {
+//   let newArr = [];
+//   for (let ele of arr) {
+//     if (ele > 2 && ele < 10 && ele % 2 === 0) {
+//       newArr.push(ele);
+//     }
+//   }
+//   console.log(newArr);
+// };
+// findNewArr([1, 3, 6, 8, 7, 10]);
+
+let array = [1, 4, 3, 6, 8, 7, 10];
+
+// using filter function
+
+let newArray = array.filter((ele) => ele > 2 && ele < 10 && ele % 2 === 0);
+console.log(newArray);
