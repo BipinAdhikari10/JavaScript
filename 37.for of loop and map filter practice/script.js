942d9139b0298619039585bd943f41618cbe47cb
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-21 14:31:22 +0545

    map for of loop and for loop practice

--- 37.for of loop and map filter practice/script.js
+++ 37.for of loop and map filter practice/script.js
@@ -10,12 +10,47 @@
 // };
 // findNewArr([1, 3, 6, 8, 7, 10]);
 
-let array = [1, 4, 3, 6, 8, 7, 10];
+// // using filter function
 
-// using filter function
+// // let newArray = array.filter((ele) => ele > 2 && ele < 10 && ele % 2 === 0);
+// // console.log(newArray);
 
-// let newArray = array.filter((ele) => ele > 2 && ele < 10 && ele % 2 === 0);
+// // let newArray = array.map((ele) => ele * 2);
+// // console.log(newArray);
+
+// // let newArray = array.map((ele) => Math.sqrt(ele));
+// // console.log(newArray);
+
+// let array = [2, 4, 5, 6];
+
+// let newArray = array.map((ele) => ele * ele);
 // console.log(newArray);
 
-let newArray = array.map((ele) => ele * 2);
-console.log(newArray);
+// Using map function
+
+let stringArray = ["Hello", "bipin", "kxa"];
+let stringLength = stringArray.map((ele) => ele.length);
+console.log(stringLength);
+
+// using for of loop
+
+const findLength = (strings) => {
+  let newArr = [];
+  for (ele of strings) {
+    let element = ele.length;
+    newArr.push(element);
+  }
+  console.log(newArr);
+};
+findLength(["hello", "kxa", "lakkua"]);
+
+// Using for loop
+let elementLength = (strings) => {
+  let newArray = [];
+  for (let i = 0; i <= strings.length - 1; i++) {
+    let elements = strings[i];
+    newArray.push(elements.length);
+  }
+  console.log(newArray);
+};
+elementLength(["Hello", "kxa", "Bipin"]);
