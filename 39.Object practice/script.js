c873ed3d12950940a861e105e5d5ef074664138c
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-23 15:16:05 +0545

    object bike models

--- 39.Object practice/script.js
+++ 39.Object practice/script.js
@@ -1,15 +1,29 @@
-const carModels = {
-  name: "Tesla",
-  color: "Red",
-  speed: "300KM/hr",
-  music: "yes",
-  fourWheels: "yes",
-  seat: "five",
-  airCondition: "yes",
-  electric: "yes",
+// const carModels = {
+//   name: "Tesla",
+//   color: "Red",
+//   speed: "300KM/hr",
+//   music: "yes",
+//   fourWheels: "yes",
+//   seat: "five",
+//   airCondition: "yes",
+//   electric: "yes",
+// };
+// carModels.autoMode = "yes";
+// // delete carModels.fourWheels;
+// for (let key in carModels) {
+//   console.log(key + ":" + carModels[key]);
+// }
+
+const bikeModels = {
+  bikeModels: "yamaha",
+  color: "black",
+  wheels: 2,
+  radio: "NO",
+  highestSpeed: "120KM/hr",
 };
-carModels.autoMode = "yes";
-// delete carModels.fourWheels;
-for (let key in carModels) {
-  console.log(key + ":" + carModels[key]);
+bikeModels.bikeName = "Fz";
+bikeModels.cc = 149;
+
+for (let key in bikeModels) {
+  console.log(key + ":" + bikeModels[key]);
 }
