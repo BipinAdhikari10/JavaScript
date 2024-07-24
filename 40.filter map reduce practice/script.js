8f7c90e99c1bd1a96cf98d27cb1b9005a64437fa
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-24 13:39:03 +0545

    product using reduce function

--- 40.filter map reduce practice/script.js
+++ 40.filter map reduce practice/script.js
@@ -32,6 +32,66 @@
 // const evenNumbers = numbers.filter((ele) => ele % 2 === 0);
 // console.log(evenNumbers);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-oddNumbers = numbers.filter((ele) => ele % 2 !== 0);
-console.log(oddNumbers);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// oddNumbers = numbers.filter((ele) => ele % 2 !== 0);
+// console.log(oddNumbers);
+
+// const products = [
+//   {
+//     product: 1,
+//     id: 1,
+//     name: "milk",
+//     price: 50,
+//   },
+//   {
+//     product: 2,
+//     id: 2,
+//     name: "diaryMilk",
+//     price: 40,
+//   },
+//   {
+//     product: 3,
+//     id: 3,
+//     name: "diaryMilk",
+//     price: 60,
+//   },
+//   {
+//     product: 3,
+//     id: 3,
+//     name: "diaryMilk",
+//     price: 90,
+//   },
+// ];
+// let result = products.filter((val) => val.price > 40);
+// console.log(result);
+
+// find the sum of all elements of an arary
+
+// let sumOfArr = (arr) => {
+//   let sum = 0;
+//   for (let i = 0; i <= arr.length - 1; i++) {
+//     sum = sum + arr[i];
+//   }
+//   console.log(sum);
+// };
+// sumOfArr([1, 2, 3, 4, 5]);
+
+// using reduce function
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = arr.reduce((ele, acc) => {
+//   return acc + ele;
+// }, 0);
+// console.log(sum);
+
+// let array = [10, 20, 30, 40];
+// let result = array.reduce((ele, acc) => {
+//   return ele + acc;
+// }, 0);
+// console.log(result);
+
+let arr = [1, 2, 3, 4];
+let resultArr = arr.reduce((ele, acc) => {
+  return ele * acc;
+}, 1);
+console.log(resultArr);
