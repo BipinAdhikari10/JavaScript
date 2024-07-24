ababd179e9e4b545e18ffff43f0fa65565ccff8b
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-24 10:20:01 +0545

    even number

--- 40.filter map reduce practice/script.js
+++ 40.filter map reduce practice/script.js
@@ -1,21 +1,33 @@
-let array = [2, 4, 6, 8, 10, 12, 15, 16, 20];
-let newArray = array.filter((val) => val > 10 && val % 2 == 0);
-console.log(newArray);
+// let array = [2, 4, 6, 8, 10, 12, 15, 16, 20];
+// let newArray = array.filter((val) => val > 10 && val % 2 == 0);
+// console.log(newArray);
 
-let data = [
-  {
-    name: "Bipin",
-    position: "developer",
-  },
+// let data = [
+//   {
+//     name: "Bipin",
+//     position: "developer",
+//   },
 
-  {
-    name: "Ram",
-    position: "driver",
-  },
-  {
-    name: "LalGopal",
-    position: "Engineer",
-  },
-];
-let dataResult = data.filter((val) => val.position === "developer");
-console.log(dataResult);
+//   {
+//     name: "Ram",
+//     position: "driver",
+//   },
+//   {
+//     name: "LalGopal",
+//     position: "Engineer",
+//   },
+// ];
+// let dataResult = data.filter((val) => val.position === "developer");
+// console.log(dataResult);
+
+let ages = [10, 20, 30];
+let result = ages.filter(checkAdult);
+console.log(result);
+
+function checkAdult(age) {
+  return age > 18;
+}
+
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter((ele) => ele % 2 === 0);
+console.log(evenNumbers);
