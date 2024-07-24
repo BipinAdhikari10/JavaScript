25be1a2f01703ddb706b0652160af311fa6abf5d
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-24 10:07:34 +0545

    filter practice

--- 40.filter map reduce practice/script.js
+++ 40.filter map reduce practice/script.js
@@ -1,3 +1,21 @@
 let array = [2, 4, 6, 8, 10, 12, 15, 16, 20];
 let newArray = array.filter((val) => val > 10 && val % 2 == 0);
 console.log(newArray);
+
+let data = [
+  {
+    name: "Bipin",
+    position: "developer",
+  },
+
+  {
+    name: "Ram",
+    position: "driver",
+  },
+  {
+    name: "LalGopal",
+    position: "Engineer",
+  },
+];
+let dataResult = data.filter((val) => val.position === "developer");
+console.log(dataResult);
