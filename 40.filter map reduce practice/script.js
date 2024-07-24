f685c46918161ba466e69a95fc4336b7784cfcf4
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-24 09:55:33 +0545

    filter

--- 40.filter map reduce practice/script.js
+++ 40.filter map reduce practice/script.js
@@ -1,3 +1,3 @@
 let array = [2, 4, 6, 8, 10, 12, 15, 16, 20];
-let newArray = array.filter((val) => val > 10);
+let newArray = array.filter((val) => val > 10 && val % 2 == 0);
 console.log(newArray);
