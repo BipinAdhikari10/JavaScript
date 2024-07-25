33afbfbb3f7b4ffec8e771e53dfe2ec4a10e6c60
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-25 10:14:14 +0545

    using splice method

--- 41.JavaScript revision/script.js
+++ 41.JavaScript revision/script.js
@@ -2,3 +2,7 @@ const number = [1, 2, 3, 4, 5];
 let removedNumber = number.pop();
 console.log(removedNumber);
 console.log(number);
+
+const fruits = ["apple", "banana", "cherry"];
+let removedFruits = fruits.splice(1, 2);
+console.log(removedFruits);
