424c0618084c0a7ccf2ab5d499b2af716afb1a23
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-26 11:45:26 +0545

    anagram

--- 42.Object and array practice/script.js
+++ 42.Object and array practice/script.js
@@ -1,46 +1,75 @@
-const myInfo = [
-  {
-    name: "Bipin",
-    city: "Galyang",
-    college: "GMC",
-    course: "BCA",
-    // nationality: "Nepali",
-  },
-  {
-    name: "Hari",
-    city: "Taplejung",
-    college: "BMC",
-    course: "BBS",
-    // nationality: "Nepali",
-  },
-  {
-    name: "Shyam",
-    city: "Dang",
-    college: "TMC",
-    course: "BSC",
-    // nationality: "Nepali",
-  },
-  {
-    name: "Ramu",
-    city: "Pokhara",
-    college: "KMC",
-    course: "BBA",
-    // nationality: "Nepali",
-  },
-];
-// let infoName = myInfo.map((ele) => {
-//   return `${ele.name} ${ele.city}`;
+// const myInfo = [
+//   {
+//     name: "Bipin",
+//     city: "Galyang",
+//     college: "GMC",
+//     course: "BCA",
+//     // nationality: "Nepali",
+//   },
+//   {
+//     name: "Hari",
+//     city: "Taplejung",
+//     college: "BMC",
+//     course: "BBS",
+//     // nationality: "Nepali",
+//   },
+//   {
+//     name: "Shyam",
+//     city: "Dang",
+//     college: "TMC",
+//     course: "BSC",
+//     // nationality: "Nepali",
+//   },
+//   {
+//     name: "Ramu",
+//     city: "Pokhara",
+//     college: "KMC",
+//     course: "BBA",
+//     // nationality: "Nepali",
+//   },
+// ];
+// // let infoName = myInfo.map((ele) => {
+// //   return `${ele.name} ${ele.city}`;
+// // });
+// // console.log(infoName);
+
+// let information = myInfo.map(function (ele) {
+//   return {
+//     myName: ele.name,
+//     myAddress: ele.city,
+//     myNationality: "Nepali",
+//   };
 // });
-// console.log(infoName);
+// console.log(information);
 
-let information = myInfo.map(function (ele) {
-  return {
-    myName: ele.name,
-    myAddress: ele.city,
-    myNationality: "Nepali",
-  };
-});
-console.log(information);
+// let subjectInfo = myInfo.filter((ele) => ele.course === "BBA");
+// console.log(subjectInfo);
 
-let subjectInfo = myInfo.filter((ele) => ele.course === "BBA");
-console.log(subjectInfo);
+const anagram = (str1, str2) => {
+  if (str1.length != str2.length) {
+    return false;
+  }
+  let count = {};
+  for (let char of str1) {
+    if (count[char]) {
+      count[char]++;
+    } else {
+      count[char] = 1;
+    }
+  }
+  for (let char of str2) {
+    if (count[char]) {
+      count[char]--;
+    } else {
+      return false;
+    }
+  }
+  for (let key in count) {
+    if (count[key] !== 0) {
+      return false;
+    }
+  }
+  return true;
+};
+let stringAnagram = anagram("Theeyes", "Theysee");
+console.log(stringAnagram);
