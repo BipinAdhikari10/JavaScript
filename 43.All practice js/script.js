837b19a6e1f5cf0d3d022b53a9263d3c5e006ca5
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-07-26 20:09:38 +0545

    reverse using an unshift

--- 43.All practice js/script.js
+++ 43.All practice js/script.js
@@ -10,5 +10,33 @@ const reverseNumber = (num) => {
   return reverse;
 };
 
-let number = reverseNumber(7891);
+let number = reverseNumber(2978);
 console.log(number);
+
+// let marks = 40;
+// let result = marks > 40 ? "paass" : "fail";
+// console.log(result);
+
+// let age = 10;
+// let vote = age > 10 ? "vote" : "no vote";
+// console.log(vote);
+
+// reverse using unshift
+
+// let reverseArray = (arr) => {
+//   let reverse = [];
+//   for (i = arr.length - 1; i >= 0; i--) {
+//     reverse.push(arr[i]);
+//   }
+//   console.log(reverse);
+// };
+// reverseArray([1, 2, 3, 4]);
+
+const reverseArray = (arr) => {
+  let reverse = [];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    reverse.unshift(arr[i]);
+  }
+  console.log(reverse);
+};
+reverseArray([1, 2, 3, 4, 5, 6, 12]);
