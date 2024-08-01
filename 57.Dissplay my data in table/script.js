ac1efa6fa50495613598df3db37810278dd04c82
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-08-01 16:22:31 +0545

    Display my data in table

---
+++ 57.Dissplay my data in table/script.js
@@ -0,0 +1,45 @@
+let data = [
+  {
+    name: "Bipin",
+    age: 23,
+    profession: "Programmer",
+  },
+];
+
+let form = document.getElementById("personal-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let inputName = document.getElementById("name");
+  let inputAge = document.getElementById("age");
+  let inputProfession = document.getElementById("profession");
+
+  let enteredName = inputName.value;
+  let enteredAge = inputAge.value;
+  let enteredProfession = inputProfession.value;
+
+  data.push({
+    name: enteredName,
+    age: enteredAge,
+    profession: enteredProfession,
+  });
+  inputName.value = "";
+  inputAge.value = "";
+  inputProfession.value = "";
+  displayData();
+});
+
+function displayData() {
+  let tableBody = document.getElementById("table-body");
+  //   console.log(tableBody);
+  let myData = "";
+  data.forEach(function (element, index) {
+    myData += `<tr>
+            <th scope="row">${index + 1}</th>
+            <td>${element.name}</td>
+            <td>${element.age}</td>
+            <td>${element.profession}</td>
+          </tr>`;
+  });
+  tableBody.innerHTML = myData;
+}
+displayData();
